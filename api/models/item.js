71117ac70c81814baa3688625fc8844bb1b2e7e7
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-16 23:41:45 -0400

    fix: reference warehouse as fkey in item

--- api/models/item.js
+++ api/models/item.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const ItemSchema = new mongoose.Schema({
   name: { type: String, required: true, index: { unique: true } },
   quantity: { type: Number, required: true, default: 0, min: 0 },
-  warehouse: { type: mongoose.Types.ObjectId, required: true },
+  warehouse: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "Warehouse",
+  },
 });
 
 ItemSchema.index({ name: 1, warehouse: 1 }, { unique: true });
