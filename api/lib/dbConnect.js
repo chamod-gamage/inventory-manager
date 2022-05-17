63bdc2d5ac32b226faa29bdfc19692cf90c376f1
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-16 20:18:18 -0400

    feat: add functionality for connecting to mongodb

---
+++ api/lib/dbConnect.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+/** 
+Source : 
+https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/utils/dbConnect.js 
+**/
+
+const MONGO_CONNECTION_URI = process.env.MONGO_CONNECTION_URI;
+
+if (!MONGO_CONNECTION_URI) {
+  throw new Error("Missing the MONGO_CONNECTION_URI environment variable");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useFindAndModify: true,
+      useCreateIndex: true,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGO_CONNECTION_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
