166b0c49a6ec5017483dfb59f7afdc618ba05ded
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-16 21:20:28 -0400

    fix: remove id from schema and prevent remodel

--- api/models/item.js
+++ api/models/item.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const ItemSchema = new mongoose.Schema({
-  id: { type: String, required: true, index: { unique: true } },
   name: { type: String, required: true, index: { unique: true } },
-  quantity: { type: Number, required: true, default: 0 },
+  quantity: { type: Number, required: true, default: 0, min: 0 },
   warehouse: { type: mongoose.Types.ObjectId, required: true },
 });
 
-const Item = mongoose.model("Item", ItemSchema);
+ItemSchema.index({ name: 1, warehouse: 1 }, { unique: true });
+
+const Item = mongoose.models.Item || mongoose.model("Item", ItemSchema);
 
 export default Item;
--- api/models/warehouse.js
+++ api/models/warehouse.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const WarehouseSchema = new mongoose.Schema({
-  id: { type: String, required: true, index: { unique: true } },
   name: { type: String, required: true, index: { unique: true } },
-  location: { type: String, required: true, index: { unique: true } },
+  location: { type: String, required: true },
 });
 
-const Warehouse = mongoose.model("Warehouse", WarehouseSchema);
+const Warehouse =
+  mongoose.models.Warehouse || mongoose.model("Warehouse", WarehouseSchema);
 
 export default Warehouse;
