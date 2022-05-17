8626420083b00b7fe1391d5e5b739916847a6d58
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-17 01:01:17 -0400

    fix: Use correct model for item creation duplication check

--- pages/api/warehouse/item/[warehouseId]/create.js
+++ pages/api/warehouse/item/[warehouseId]/create.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
   const { warehouseId } = req.query;
   try {
-    if ((await Warehouse.find({ name, warehouse: warehouseId })).length)
+    if ((await Item.find({ name, warehouse: warehouseId })).length)
       throw new Error("Item already exists");
     const item = await Item.create({ name, quantity, warehouse: warehouseId });
     res.status(200).json(item);
